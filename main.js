948a6c9fa4d1da2e174ca40a45f9e5db9cef4416
dartiros <100169417+realdarter@users.noreply.github.com>
2024-02-29 15:13:59 -0800

    Added Debouncer
    Edited some text and added a debouncer

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
+var isButtonClicked = false;
+
 function myFunction() {
+    if (isButtonClicked) {
+        return;
+    }
+    // script.js
+    console.log("Hello, this message will be printed to the terminal.");
+
+    isButtonClicked = true;
     var textElement = document.getElementById("myText");
     
     // Change the text
-    textElement.innerText = "Im hungry as fuck gimme food pls!";
+    textElement.innerText = "I'm hungry as heck, give me food, please!";
 
     // Display the text
     textElement.style.display = "block";
@@ -11,5 +20,6 @@ function myFunction() {
     setTimeout(function() {
         textElement.innerText = "Original Text";
         textElement.style.display = "none";
+        isButtonClicked = false; // Reset isButtonClicked to false after the timeout
     }, 2000);
 }
